ce3e4f816c9032477b5d512fda10781b74395981
YaxinWangUT <104438995+YaxinWangUT@users.noreply.github.com>
2022-04-26 21:05:17 +0800

    Update CreatePoll.js
    basic function done, 
    data transfer tbd...

--- vote-app/src/components/CreatePoll.js
+++ vote-app/src/components/CreatePoll.js
@@ -1,9 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add"
+import DeleteIcon from "@mui/icons-material/Delete"
+
+const mdTheme = createTheme({
+    palette: {
+      primary: { main: "#e91e63", contrastText: "#fff" },
+      secondary: { main: "#616161", contrastText: "#fff" },
+      blue: { main: "#2196f3", contrastText: "#fff" },
+    },
+  });
 
 export default function CreatePoll() {
+    const [options,setOptions] = useState([
+        {option: ''},
+        {option: ''},
+    ])
+    const handleSubmit = (event) => {
+        console.log("poll created")
+    }
+    const handleAddOption = (event) => {
+        setOptions([...options,{option: ''}])
+    }
+    const handleDropOption = (index) => {
+        const values=[...options];
+        values.splice(index,1);
+        setOptions(values);
+    }
+
     return (
-        <React.Fragment>
-            {"Edit here"}
-        </React.Fragment>
+    <ThemeProvider theme={mdTheme}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <CssBaseline />
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="flex-start"
+          style={{ minHeight: "100vh" }}
+        >
+        <Typography variant="h4" >
+          create new Poll
+        </Typography>
+        <hr/>
+        <Typography variant="h5" >
+          basic informations:
+        </Typography>
+        
+        <form>
+            <div><TextField id="descript" label="decription" variant="filled"/></div>
+            <div><TextField id="time1" label="starting time" variant="filled"/></div>
+            <div><TextField id="time2" label="registration end time" variant="filled"/></div>
+            <div><TextField id="time3" label="voting end time" variant="filled"/></div>
+            <div><TextField id="bar" label="voting bar" variant="filled"/></div>
+            <hr/>
+            <Typography variant="h5" >designing options:</Typography>
+            {options.map((option,index)=>(
+                <div key={index}>
+                    <TextField id="option" label="option" variant="standard" value={option.option}/>
+                    <IconButton onClick={handleAddOption}><AddIcon/></IconButton>
+                    <IconButton onClick={handleDropOption}><DeleteIcon/></IconButton>
+                </div>
+            ))}
+        
+        </form>
+        <hr/>
+        <Button type="submit"  onClick={handleSubmit}  variant="contained"  color = "blue">create</Button>
+        </Grid>
+      </Box>
+    </ThemeProvider>
       );
 }
