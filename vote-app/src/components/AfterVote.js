2dc99442169fd2094c360d2451f949c04689230c
YaxinWangUT <104438995+YaxinWangUT@users.noreply.github.com>
2022-04-26 23:38:57 +0800

    Update AfterVote.js

--- vote-app/src/components/AfterVote.js
+++ vote-app/src/components/AfterVote.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Typography from "@mui/material/Typography";
 import { useNavigate } from 'react-router-dom';
 import Button from "@mui/material/Button";
+import Container from '../container';
+import { Grid } from '@mui/material';
 
 export default function AfterVote() {
     let navigate = useNavigate();
@@ -9,24 +11,14 @@ export default function AfterVote() {
       event.preventDefault();
       navigate('/home');
     }
-        return (
-            <div>
-            <Typography variant="h4" mt={6} >
-            {"Thank you for your participation!"}
-            {"You may check the result after this voting end."}
-            </Typography>
-            <Button
-              type="submit"
-              onClick={handleSubmit}
-              fullWidth
-              variant="contained"
-              color = "blue"
-              sx={{ mt: 15, mb: 2, maxWidth: '300px', fontSize: '30px'}}
-            >
-              Back to homepage
-            </Button>
-            </div>
+
+    return (
+        <Grid alignItems="center">
+            <Typography variant="h4" mt={6} >{"Thank you for your participation!"}</Typography>
+            <Typography variant="h4" mt={4} >{"You may check the result after this voting end."}</Typography>
+            <Container component="main" sx={{ mt: 20, mb: 2 }} maxWidth="lg"> </Container>
+            <Button type="submit"onClick={handleSubmit} variant="contained" sx={{mt:20, maxWidth: '300px'}}>Back to homepage</Button>
+
+        </Grid>
         );
     }
-
- 
